840f9613341bee4697bd4faa5277cb310f43ed7b
vinayrk77 <vinayrk26@gmail.com>
2025-05-23 20:57:43 +0530

    test01 class project

---
+++ test02.java
@@ -0,0 +1,10 @@
+public class test02 {
+    public static void main(String [] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/3);
+        System.out.println(4/3);
+
+    }
+}
